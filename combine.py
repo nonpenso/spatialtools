0fcc19237082c81869311afb7ab2c0408a52c90e
Giovanni <giovanni.caudullo@gmail.com>
2014-10-01 11:58:44 +0200

    Update combine.py

--- combine.py
+++ combine.py
@@ -10,13 +10,21 @@ def combine(array1, array2):
     q2 = list(numpy.unique(array2))
     comb = list(itertools.product(q1,q2))
 
+    keys = {}
     flatarr = numpy.array([], dtype=numpy.int16)
     for nd, val1 in numpy.ndenumerate(array1):
         val2 = array2[nd[0],nd[1]]
         ind = comb.index((val1, val2))
         flatarr = numpy.append(flatarr, ind)
-    outarr = flatarr.reshape(array2.shape)
-    return outarr
+        if not keys.has_key(ind):
+            keys[ind]=(val1,val2)
+
+    rank = {e:n for n,e in enumerate(numpy.unique(flatarr))}
+    out1 = numpy.array([], dtype=numpy.int16)
+    for x in flatarr:
+        out1 = numpy.append(out1, rank[x])    
+    outarr = out1.reshape(array2.shape)
+    return outarr, keys
 
 
 # TEST
@@ -30,13 +38,14 @@ a2 = numpy.array([[ 2, 2, 3, 3],
                   [ 3, 3, 3, 3],
                   [ 3, 3, 3, 3]])
 
-output = combine(a1,a2)
+output, k = combine(a1,a2)
 print output
+print k
 
 '''
-output:
->>> array([[0, 0, 1, 3],
-           [0, 0, 1, 3],
-           [1, 1, 1, 3],
-           [3, 3, 3, 3]])
+([[0, 0, 1, 2],
+ [0, 0, 1, 2],
+ [1, 1, 1, 2],
+ [2, 2, 2, 2]])
+{0: (0, 2), 1: (0, 3), 3: (1, 3)}
 '''
