5ba8c0598148aab563dbc658720de79b74f78f5a
Giovanni <giovanni.caudullo@gmail.com>
2014-10-01 15:31:16 +0200

    Update combine.py

--- combine.py
+++ combine.py
@@ -6,25 +6,16 @@ def combine(array1, array2):
     assert isinstance(array1, numpy.ndarray), 'Input must be numpy arrays'
     assert isinstance(array2, numpy.ndarray), 'Input must be numpy arrays'
 
-    q1 = list(numpy.unique(array1))
-    q2 = list(numpy.unique(array2))
-    comb = list(itertools.product(q1,q2))
-
-    keys = {}
     flatarr = numpy.array([], dtype=numpy.int16)
+    indlist = []    
     for nd, val1 in numpy.ndenumerate(array1):
         val2 = array2[nd[0],nd[1]]
-        ind = comb.index((val1, val2))
+        if (val1,val2) not in indlist:
+            indlist.append((val1,val2))
+        ind = indlist.index((val1,val2))
         flatarr = numpy.append(flatarr, ind)
-        if not keys.has_key(ind):
-            keys[ind]=(val1,val2)
-
-    rank = {e:n for n,e in enumerate(numpy.unique(flatarr))}
-    out1 = numpy.array([], dtype=numpy.int16)
-    for x in flatarr:
-        out1 = numpy.append(out1, rank[x])    
-    outarr = out1.reshape(array2.shape)
-    return outarr, keys
+    outarr = flatarr.reshape(array2.shape)
+    return outarr
 
 
 # TEST
@@ -38,9 +29,9 @@ a2 = numpy.array([[ 2, 2, 3, 3],
                   [ 3, 3, 3, 3],
                   [ 3, 3, 3, 3]])
 
-output, k = combine(a1,a2)
+output = combine(a1,a2)
 print output
-print k
+
 
 '''
 ([[0, 0, 1, 2],
